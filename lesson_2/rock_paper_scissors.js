5e91e82b0f0d81bdd00538a5e91e9d3a715cd724
Erin Davidson <rosemary.erin.o@gmail.com>
2023-01-13 18:36:41 +0000

    create rock, paper, scissors game

---
+++ lesson_2/rock_paper_scissors.js
@@ -0,0 +1,50 @@
+const readline = require('readline-sync');
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_ANSWERS = ['y', 'n'];
+
+function prompt(message) {
+  console.log(`==> ${message}`);
+}
+
+function randomIndex() {
+  return Math.floor(Math.random() * VALID_CHOICES.length);
+}
+
+function userWon(uChoice, cChoice) {
+  return ((uChoice === 'rock') && (cChoice === 'scissors') ||
+      (uChoice === 'paper') && (cChoice === 'rock') ||
+      (uChoice === 'scissors') && (cChoice === 'paper'));
+}
+
+function playAgain() {
+  prompt('Do you want to play again? Enter "y" or "n":');
+  let answer = readline.question().trim().toLowerCase();
+  while (!VALID_ANSWERS.includes(answer)) {
+    prompt('enter a valid y/n value:')
+    answer = readline.question().trim().toLowerCase();
+  }
+  return answer === 'y' ? true : false;
+}
+
+do {
+  console.clear();
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let userChoice = readline.question();
+  
+  while (!VALID_CHOICES.includes(userChoice)) {
+    prompt('Enter a valid choice (rock, paper, scissors):');
+    userChoice = readline.question();
+  }
+  
+  let computerChoice = VALID_CHOICES[randomIndex()];
+  
+  prompt(`User chose: ${userChoice}, computer chose ${computerChoice}`);
+  
+  if (userWon(userChoice, computerChoice)) {
+    prompt('User won!');
+  } else if (userChoice === computerChoice) {
+    prompt('You tied with the computer!');
+  } else {
+    prompt('The computer won this round... :(');
+  }
+} while (playAgain());
