7d135edb8cc00eb71bf58d3218a16272cb8c9949
Erin Davidson <rosemary.erin.o@gmail.com>
2023-01-24 19:50:15 +0000

    add julian calendar functionality

--- small_problems/easy_1/leapYearsPt1.js
+++ small_problems/easy_1/leapYearsPt1.js
@@ -59,6 +59,9 @@ your solution.
             return true
 */
 function isLeapYear(year) {
+  if (year <= 1752) {
+    return (year % 4 === 0);
+  }
   if (year % 4 === 0) {
     if (year % 100 === 0) {
       return (year % 400 === 0); 
